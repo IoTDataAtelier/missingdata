be2b46e743f4cfcfc648ad696097c78fa281317b
rayssadavvila <rayssamunizd@gmail.com>
2025-07-29 22:23:27 -0300

    Refactoring and organizing code

---
+++ MissingDataNew/Experiments/imputed_data/imputed_data.py
@@ -0,0 +1,157 @@
+import os
+import sys
+import pypots
+import numpy as np
+import benchpots
+import matplotlib.pyplot as plt
+from pypots.optim import Adam
+from pypots.imputation import SAITS, BRITS, USGAN, GPVAE, MRNN
+from pypots.utils.random import set_random_seed
+module_path = os.path.abspath(os.path.join('..'))
+if module_path not in sys.path:
+    sys.path.append(module_path)
+
+
+set_random_seed()
+physionet2012_dataset = benchpots.datasets.preprocess_physionet2012(subset="all", rate=0.1)
+print(physionet2012_dataset.keys())
+
+dataset_for_IMPU_training = {
+    "X": physionet2012_dataset['train_X'],
+}
+
+dataset_for_IMPU_validating = {
+    "X": physionet2012_dataset['val_X'],
+    "X_ori": physionet2012_dataset['val_X_ori'],
+}
+
+dataset_for_IMPU_testing = {
+    "X": physionet2012_dataset['test_X'],
+}
+
+test_X_indicating_mask = np.isnan(physionet2012_dataset['test_X_ori']) ^ np.isnan(physionet2012_dataset['test_X'])
+test_X_ori = np.nan_to_num(physionet2012_dataset['test_X_ori'])  
+
+saits = SAITS(
+    n_steps=physionet2012_dataset['n_steps'],
+    n_features=physionet2012_dataset['n_features'],
+    n_layers=1,
+    d_model=256,
+    d_ffn=128,
+    n_heads=4,
+    d_k=64,
+    d_v=64,
+    dropout=0.1,
+    ORT_weight=1,  
+    MIT_weight=1,
+    batch_size=32,
+    epochs=10,
+    patience=3,
+    optimizer=Adam(lr=1e-3),
+    num_workers=0,
+    device=None,
+    model_saving_strategy="best",
+)
+
+brits = BRITS(
+    n_steps=physionet2012_dataset['n_steps'],
+    n_features=physionet2012_dataset['n_features'],
+    rnn_hidden_size=128,
+    batch_size=32,
+    epochs=10,
+    patience=3,
+    optimizer=Adam(lr=1e-3),
+    num_workers=0,
+    device=None,
+    model_saving_strategy="best",
+)
+
+us_gan = USGAN(
+    n_steps=physionet2012_dataset['n_steps'],
+    n_features=physionet2012_dataset['n_features'],
+    rnn_hidden_size=256,
+    lambda_mse=1,
+    dropout=0.1,
+    G_steps=1,
+    D_steps=1,
+    batch_size=32,
+    epochs=10,
+    patience=3,
+    G_optimizer=Adam(lr=1e-3),
+    D_optimizer=Adam(lr=1e-3),
+    num_workers=0,
+    device=None,
+    model_saving_strategy="best",
+)
+
+gp_vae = GPVAE(
+    n_steps=physionet2012_dataset['n_steps'],
+    n_features=physionet2012_dataset['n_features'],
+    latent_size=37,
+    encoder_sizes=(128,128),
+    decoder_sizes=(256,256),
+    kernel="cauchy",
+    beta=0.2,
+    M=1,
+    K=1,
+    sigma=1.005,
+    length_scale=7.0,
+    kernel_scales=1,
+    window_size=24,
+    batch_size=32,
+    epochs=10,
+    patience=3,
+    optimizer=Adam(lr=1e-3),
+    num_workers=0,
+    device=None,
+    model_saving_strategy="best",
+)
+
+mrnn = MRNN(
+    n_steps=physionet2012_dataset['n_steps'],
+    n_features=physionet2012_dataset['n_features'],
+    rnn_hidden_size=128,
+    epochs=10,
+    patience=3,
+    optimizer=Adam(lr=1e-3),
+    num_workers=0,
+    device=None,
+    model_saving_strategy="best",
+)
+
+saits.load("../mae/tutorial_results/imputation/saits/20250422_T181642/SAITS.pypots")
+
+brits.load("../mae/tutorial_results/imputation/brits/20250422_T181643/BRITS.pypots")
+
+us_gan.load("../mae/tutorial_results/imputation/us_gan/20250422_T181643/USGAN.pypots")
+
+gp_vae.load("../mae/tutorial_results/imputation/gp_vae/20250422_T181643/GPVAE.pypots")
+
+mrnn.load("../mae/tutorial_results/imputation/mrnn/20250422_T181643/MRNN.pypots")
+
+
+saits_results = saits.predict(dataset_for_IMPU_testing)
+saits_imputation = saits_results["imputation"]
+
+np.save('physionet_saits', saits_imputation)
+
+brits_results = brits.predict(dataset_for_IMPU_testing)
+brits_imputation = brits_results["imputation"]
+
+np.save('physionet_brits', brits_imputation)
+
+us_gan_results = us_gan.predict(dataset_for_IMPU_testing)
+us_gan_imputation = us_gan_results["imputation"]
+
+np.save('physionet_usgan', us_gan_imputation)
+
+gp_vae_results = gp_vae.predict(dataset_for_IMPU_testing)
+gp_vae_imputation = gp_vae_results["imputation"]
+
+np.save('physionet_gpvae', gp_vae_imputation)
+
+mrnn_results = mrnn.predict(dataset_for_IMPU_testing)
+mrnn_imputation = mrnn_results["imputation"]
+
+np.save('physionet_mrnn', mrnn_imputation)
+
