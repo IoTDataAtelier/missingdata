6160e7b8655d0d59b5f4f9fb9781e7ec0fe84c0b
rayssadavvila <rayssamunizd@gmail.com>
2025-11-05 21:15:29 -0300

    Modifying the calculateparameters file

--- MissingDataNew/Experiments/calculateParameters.py
+++ MissingDataNew/Experiments/calculateParameters.py
@@ -4,6 +4,7 @@ import numpy as np
 import benchpots
 from pypots.optim import Adam
 from pypots.imputation import SAITS, BRITS, USGAN, GPVAE, MRNN
+from Models import Models
 from pypots.utils.random import set_random_seed
 module_path = os.path.abspath(os.path.join('..'))
 if module_path not in sys.path:
@@ -29,6 +30,17 @@ dataset_for_IMPU_testing = {
 test_X_indicating_mask = np.isnan(physionet2012_dataset['test_X_ori']) ^ np.isnan(physionet2012_dataset['test_X'])
 test_X_ori = np.nan_to_num(physionet2012_dataset['test_X_ori'])  
 
+models = Models()
+
+saits = models.model("saits", dataset=physionet2012_dataset, train=False)
+
+brits = models.model("brits", dataset=physionet2012_dataset, train=False)
+
+us_gan = models.model("usgan", dataset=physionet2012_dataset, train=False)
+
+gp_vae = models.model("gpvae", dataset=physionet2012_dataset, train=False)
+
+mrnn = models.model("mrnn", dataset=physionet2012_dataset, train=False)
 
 def get_total_params (model_x):
     total_params = sum(p.numel() for p in model_x.model.parameters() if p.requires_grad)
